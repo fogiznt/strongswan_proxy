aac1509a0a794d3244d0f806baa9b0652141e2fa
fogiznt <85696249+fogiznt@users.noreply.github.com>
2021-08-18 15:32:43 +0500

    Update proxy_manager.sh

--- proxy_manager.sh
+++ proxy_manager.sh
@@ -22,12 +22,14 @@ proxy_port=${proxy_port##*=}
 proxy_port=$(shuf -i $proxy_port -n 1)
 speed=$(sed -n 6p /root/strong_proxy/settings.txt)
 speed=${speed##*=}
-speed=$(($speed*1024/40))
+speed=$(($speed*1024/30))
 
+echo $ip > /root/strong_proxy/ip_$client_ip
+echo $local_port > /root/strong_proxy/local_port_$client_ip
 
 docker run --name $user -d --net=host --privileged -e socks5 $image_id -a 0.0.0.0 -p $local_port -t $server_domain:$proxy_port
-tc class add dev eth0 parent 1: classid 1:$local_port htb rate $speedkbit quantum 3000
-tc filter add dev eth0 parent 1: protocol ip handle $local_port fw flowid 1:$local_port
+tc class add dev eth0 parent 1: classid 1:$local_port htb rate $speed\kbit quantum 3000
+tc filter add dev eth0 parent 1: protocol ip pref $local_port handle $local_port fw flowid 1:$local_port
 iptables -t mangle -I PREROUTING 1 -s $ip -j MARK --set-mark $local_port
 iptables -t nat -I PREROUTING 1 -s $ip -p tcp -j REDIRECT --to-ports $local_port
 
@@ -54,8 +56,16 @@ server_ip=${server_ip##*=}
 client_ip=$(tail -n 10 /var/log/syslog | grep "deleting IKE_SA ikev2-vpn" | grep -Eo "([0-9]{1,3}[\.]){3}[0-9]{1,3}" | grep -v "$server_ip")
 user=$client_ip
 sed -i '2d' /root/strong_proxy/killproxy_$user
-
 /root/strong_proxy/killproxy_$user
+
+ip=$(echo /root/strong_proxy/ip_$client_ip)
+local_port=$(echo /root/strong_proxy/local_port_$client_ip)
+
+tc filter del dev eth0 parent 1: protocol ip pref $local_port
+tc class delete dev eth0 parent 1: classid 1:$local_port
+iptables -t mangle -D PREROUTING -s $ip -j MARK --set-mark $local_port 
+
+
 num=$(grep -n "$user" /var/www/html/clients | cut -b -1)
 sed -i $num'd' /var/www/html/clients
 kill_prev=$(pidof $(ps -uax | grep "/bin/sh /root/killproxy_$user"))
